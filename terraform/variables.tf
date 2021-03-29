b19866c91c6bfd1480d1b4a8244c4b7a8a826167
ejbest <20603184+ejbest@users.noreply.github.com>
2021-03-29 07:53:11 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
-  default = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  default    = "us-east-1"
+  access_key = "var.access_key"
+  secret_key = "var.secret_key"
 }
 
 variable "cluster-name" {
